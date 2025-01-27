c2e1da1298a63745dd738bb664a6637402882a9f
AhmedBnSamy <samy4118@gmail.com>
2025-01-28 01:10:18 +0200

    first commit

---
+++ lib/cubits/register_cubit/register_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:meta/meta.dart';
+
+part 'register_state.dart';
+class RegisterCubit extends Cubit<RegisterState> {
+  RegisterCubit() : super(RegisterInitial());
+
+  Future<User?> register(String email, String password) async {
+    emit(RegisterLoading());
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      emit(RegisterSuccess(userCredential.user!));
+      return userCredential.user;
+    } catch (e) {
+      emit(RegisterError(
+        message: e.toString(),
+      ));
+      return null;
+    }
+  }
+}
